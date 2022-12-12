2aa6c8c9fb41e85cafff0356394dc3997a71389b
Nathan GD <nathan.gardet-derc@pm.me>
2022-12-12 17:09:18 +0000

    updated redux store to fit our data. fixed cart

--- front-end/src/reducers/productReducer.js
+++ front-end/src/reducers/productReducer.js
@@ -1,39 +1,5 @@
 import { ADD_TO_CART, GET_NUMBERS_BASKET, INCREASE_QUANTITY, DECREASE_QUANTITY, CLEAR_PRODUCT } from '../actions/types';
-
-const initialState = {
-    cart: 0,
-    cartPrice: 0,
-    products: {
-        moong: {
-            name: "Moong",
-            price: 12.00,
-            tagName: "moong",
-            numbers: 0,
-            inCart: false
-        },
-        sunflower: {
-            name: "Sunflower oil",
-            price: 70.00,
-            tagName: "sunflower",
-            numbers: 0,
-            inCart: false
-        },
-        kabuli: {
-            name: "Kabuli Chana(1 kg)",
-            price: 80.00,
-            tagName: "kabuli",
-            numbers: 0,
-            inCart: false
-        },
-        soyachunks: {
-            name: "Soya Chunks(1 kg)",
-            price: 90.00,
-            tagName: "soyachunks",
-            numbers: 0,
-            inCart: false
-        }
-    }
-}
+import { get_price_range } from "../products.ts";
 
 /**
 
@@ -42,87 +8,100 @@ const initialState = {
 cart: total number of items in cart
 cartPrice: total price of items in cart
 products: object containing all the products
-products: {
-  name: name of the product
-}
+products: [
+  name: {
+    price: price range of the product
+    numbers: number of items in cart
+  }
+]
 
 **/
 
+const initialState = {
+  cart: 0,
+  cartPrice: [0, 0],
+  products: {
+  }
+};
+
 export default (state = initialState, action) => {
-    let productSelected = "";
-    switch (action.type) {
-        case ADD_TO_CART:
-            productSelected = { ...state.products[action.payload] }
-            productSelected.numbers += 1;
-            productSelected.inCart = true;
+  let productSelected = "";
+  switch (action.type) {
+    case ADD_TO_CART:
+      let price = get_price_range(action.payload);
+      productSelected = state.products[action.payload] || {
+        price: [price[0], price[1]],
+        numbers: 0,
+      };
+      productSelected.numbers += 1;
 
-            return {
-                ...state,
-                cart: state.cart + 1,
-                cartPrice: state.cartPrice + state.products[action.payload].price,
-                products: {
-                    ...state.products,
-                    [action.payload]: productSelected
-                }
-            }
-        case GET_NUMBERS_BASKET:
-            return {
-                ...state
-            }
+      return {
+        ...state,
+        cart: state.cart + 1,
+        cartPrice: [state.cartPrice[0] + price[0], state.cartPrice[1] + price[1]],
+        products: {
+          ...state.products,
+          [action.payload]: productSelected
+        }
+      };
+    case GET_NUMBERS_BASKET:
+      return {
+        ...state
+      };
 
-        case INCREASE_QUANTITY:
-            productSelected = { ...state.products[action.payload] }
-            productSelected.numbers += 1;
-            return {
-                ...state,
-                cart: state.cart +1 ,
-                cartPrice: state.cartPrice + state.products[action.payload].price,
-                products: {
-                    ...state.products,
-                    [action.payload]: productSelected
-                }
-            }
+    case INCREASE_QUANTITY:
+      productSelected = { ...state.products[action.payload] };
+      productSelected.numbers += 1;
+      return {
+        ...state,
+        cart: state.cart + 1,
+        cartPrice: [state.cartPrice[0] + state.products[action.payload].price[0], state.cartPrice[1] + state.products[action.payload].price[1]],
+        products: {
+          ...state.products,
+          [action.payload]: productSelected
+        }
+      };
 
-        case DECREASE_QUANTITY:
-            productSelected = { ...state.products[action.payload] }
-            let newCartPrice = 0;
-            let newCartNumbers = 0;
-            if (productSelected.numbers === 0) {
-                productSelected.numbers = 0
-                newCartPrice = state.cartPrice
-                newCartNumbers = state.cart
-            }
-            else {
-                productSelected.numbers -= 1;
-                newCartPrice = state.cartPrice - state.products[action.payload].price
-                newCartNumbers = state.cart - 1
-            }
-            return {
-                ...state,
-                cart: newCartNumbers ,
-                cartPrice: newCartPrice,
-                products: {
-                    ...state.products,
-                    [action.payload]: productSelected
-                }
-            }
+    case DECREASE_QUANTITY:
+      productSelected = { ...state.products[action.payload] };
+      let newCartPrice = 0;
+      let newCartNumbers = 0;
+      if (productSelected.numbers === 0) {
+        productSelected.numbers = 0;
+        newCartPrice = state.cartPrice;
+        newCartNumbers = state.cart;
+      }
+      else {
+        productSelected.numbers -= 1;
+        newCartPrice = [state.cartPrice[0] - state.products[action.payload].price[0], state.cartPrice[1] - state.products[action.payload].price[1]];
+        newCartNumbers = state.cart - 1;
+      }
+      return {
+        ...state,
+        cart: newCartNumbers,
+        cartPrice: newCartPrice,
+        products: {
+          ...state.products,
+          [action.payload]: productSelected
+        }
+      };
 
-        case CLEAR_PRODUCT:
-            productSelected = { ...state.products[action.payload] };
-            let numbersBackup = productSelected.numbers;
-            productSelected.numbers = 0 ;
-            productSelected.inCart = false
-            return {
-                ...state,
-                cartPrice: state.cartPrice - (numbersBackup * productSelected.price),
-                cart: state.cart - numbersBackup,
-                products: {
-                    ...state.products,
-                    [action.payload]: productSelected
-                }
-            }
+    case CLEAR_PRODUCT:
+      productSelected = { ...state.products[action.payload] };
+      let _price = productSelected.price;
+      let numbers = productSelected.numbers;
+      delete state.products[action.payload];
+      return {
+        ...state,
+        cartPrice: [state.cartPrice[0] - _price[0] * numbers, state.cartPrice[1] - _price[1] * numbers],
+        cart: state.cart - numbers,
+        products: {
+          ...state.products,
+          [action.payload]: productSelected
+        }
+      };
 
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};

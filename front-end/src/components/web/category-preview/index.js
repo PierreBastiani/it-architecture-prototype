f9b475e91837948e4f3f9f16458e9584755bafa7
PierreBastiani <pambastiani@gmail.com>
2022-12-13 00:24:02 +0100

    new thumbnails

--- front-end/src/components/web/category-preview/index.js
+++ front-end/src/components/web/category-preview/index.js
@@ -7,7 +7,7 @@ export default class CategoryPreview extends Component {
             <div className="col-md-3 m-wthree" style={{"paddingBottom": "3rem"}}>
                 <div className="col-m">
                     <Link to="#" data-toggle="modal" data-target={`#myModal${this.props.index + 1}`} className="offer-img">
-                        <img src={`${this.props.category.thumbnail_url}`} className="img-responsive" alt="" />
+                        <img src={`${this.props.category.thumbnail_url}`} className="img-responsive" alt="" style={{}}/>
                     </Link>
                     <div className="mid-1">
                         <div className="women">
